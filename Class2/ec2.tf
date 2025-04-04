6b6a78e07c79987c0844707841d48f50aa08da12
BibigulKosherova <bibigulkosherova@gmail.com>
2025-04-04 01:53:51 +0000

    added class2 files

---
+++ Class2/ec2.tf
@@ -0,0 +1,18 @@
+resource "aws_instance" "web" {
+  ami           = "ami-00a929b66ed6e0de6"
+  instance_type = "t2.micro"
+  #availability_zone = "us-east-1d"
+  subnet_id = "subnet-08495f5f855c07d01"
+  key_name = aws_key_pair.deployer.key_name
+  vpc_security_group_ids = [aws_security_group.allow_tls.id]
+  user_data = file("apache.sh")
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
+
+
+output e2_public_ip {
+    value = aws_instance.web.public_ip
+}
